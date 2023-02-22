c72c8d085449c4ce843401943d7d497fc4d23720
santos-eric <esantosdev77@gmail.com>
2023-02-22 14:11:32 -0700

    create vite react, sass  and workday canvas skeleton

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {PrimaryButton} from '@workday/canvas-kit-react/button';
+
+function App() {
+
+
+  return (
+    <>
+      <PrimaryButton variant='inverted'>
+        Enter
+      </PrimaryButton>
+    </>
+  )
+}
+
+export default App
