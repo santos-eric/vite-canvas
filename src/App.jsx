e32907331664e959d3d8745474b4473a7991cb66
santos-eric <esantosdev77@gmail.com>
2023-04-19 04:48:26 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import './App.css'
-import {PrimaryButton} from '@workday/canvas-kit-react/button';
+// import {PrimaryButton} from '@workday/canvas-kit-react/button';
+import { gsap } from 'gsap'
+import imagesLoaded from 'imagesloaded'
+import photo1 from '../assets/aguachiles.jpg'
+import photo2 from '../assets/austinDowntown.jpg'
+import photo3 from '../assets/bal-downtown.jpg'
+import photo4 from '../assets/aguachiles.jpg'
+import photo5 from '../assets/aguachiles.jpg'
 
 function App() {
+
+  const content = document.querySelector('section')
+
+  // get all images
+  const imgLoad = imagesLoaded(content)
+
+  // images loaded, loader disappears
+  imgLoad.on('done', (instance) => {
+    gsap.to('.blinder', {
+      scaleY: 0,
+      stagger: .03,
+      ease: 'power3.out'
+    })
+  })
 
 
   return (
     <>
-      <PrimaryButton variant='inverted'>
-        Enter
-      </PrimaryButton>
+      <div className="loader">
+        <div className="blinder-container">
+          <div className="blinder"></div>
+          <div className="blinder"></div>
+          <div className="blinder"></div>
+          <div className="blinder"></div>
+          <div className="blinder"></div>
+        </div>
+      </div>
+
+      <div className="section">
+        <img src='../assets/aguachiles.jpg' alt="" />
+        <img src={photo2} alt="" />
+        <img src={photo3} alt="" />
+        <img src={photo4} alt="" />
+        <img src={photo5} alt="" />
+      </div>
     </>
   )
 }
